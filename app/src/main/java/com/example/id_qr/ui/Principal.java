f0388ed5774c9ea0bd44bd198034ee32ffcee79b
andre <andres.hoyos18@eia.edu.co>
2020-11-11 00:37:24 -0500

    November 11 2020, 12:37 am

--- app/src/main/java/com/example/id_qr/ui/Principal.java
+++ app/src/main/java/com/example/id_qr/ui/Principal.java
@@ -1,6 +1,9 @@
 package com.example.id_qr.ui;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -8,6 +11,10 @@ import androidx.navigation.ui.NavigationUI;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.example.id_qr.R;
@@ -19,6 +26,14 @@ public class Principal extends AppCompatActivity {
     private BottomNavigationView navView;
     private long backPressedTime;
     private Toast backToast;
+    private Handler mHandler;
+
+    private final Fragment fragment1 = new QRFragment();
+    private final Fragment fragment2 = new PagoFragment();
+    private final Fragment fragment3 = new RecargaFragment();
+    private final Fragment fragment4 = new HistorialFragment();
+    private FragmentManager fm = getSupportFragmentManager();
+    private Fragment active = fragment1;
 
 
     @Override
@@ -27,6 +42,18 @@ public class Principal extends AppCompatActivity {
         setContentView(R.layout.activity_principal);
 
         navView = findViewById(R.id.bottomNavigationView);
+
+
+        mHandler = new Handler();
+
+
+        BottomNavigationView navigation = findViewById(R.id.bottomNavigationView);
+        /*usar esto en vez de Navcontroller y NavigationUI*/
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragment4, "4").hide(fragment4).commit();
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragment3, "3").hide(fragment3).commit();
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragment2, "2").hide(fragment2).commit();
+        fm.beginTransaction().add(R.id.nav_host_fragment, fragment1, "1").commit();
         /*
          * Usar esto si en el manifesto hay soporte para la actionBar
          */
@@ -34,12 +61,15 @@ public class Principal extends AppCompatActivity {
         //        R.id.fragmentQR, R.id.fragmentPago, R.id.fragmentRecarga, R.id.fragmentHistorial)
         //        .build();
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        //// NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         /*
          * Usar esto si en el manifesto hay soporte para la actionBar
          */
         //NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
-        NavigationUI.setupWithNavController(navView, navController);
+
+        ////NavigationUI.setupWithNavController(navView, navController);
+
+        repeater();
 
     }
 
@@ -70,4 +100,49 @@ public class Principal extends AppCompatActivity {
             navView.setSelectedItemId(R.id.fragmentQR);
         }
     }
+
+    public void repeater() {
+        Log.i("TAG", "repeater");
+        mHandler.postDelayed(runable, 10);
+
+    }
+
+    public Runnable runable = new Runnable() {
+        @Override
+        public void run() {
+            Log.i("TAG", "Runable");
+            QRFragment.refrescarQR();
+            int segundos = 10;
+            mHandler.postDelayed(this, 1000 * segundos);
+        }
+    };
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.fragmentQR:
+                    fm.beginTransaction().hide(active).show(fragment1).commit();
+                    active = fragment1;
+                    return true;
+
+                case R.id.fragmentPago:
+                    fm.beginTransaction().hide(active).show(fragment2).commit();
+                    active = fragment2;
+                    return true;
+
+                case R.id.fragmentRecarga:
+                    fm.beginTransaction().hide(active).show(fragment3).commit();
+                    active = fragment3;
+                    return true;
+                case R.id.fragmentHistorial:
+                    fm.beginTransaction().hide(active).show(fragment4).commit();
+                    active = fragment4;
+                    return true;
+            }
+            return false;
+        }
+    };
 }
--- app/src/main/java/com/example/id_qr/ui/QRFragment.java
+++ app/src/main/java/com/example/id_qr/ui/QRFragment.java
@@ -1,14 +1,24 @@
 package com.example.id_qr.ui;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.id_qr.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.util.Random;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +26,10 @@ import com.example.id_qr.R;
  * create an instance of this fragment.
  */
 public class QRFragment extends Fragment {
+
+    private static final String TAG = "QR_Fragment";
+
+    private static ImageView qrImageView;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -55,12 +69,47 @@ public class QRFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_q_r, container, false);
+        qrImageView = (ImageView) view.findViewById(R.id.qr_imageView);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_q_r, container, false);
+        //return inflater.inflate(R.layout.fragment_q_r, container, false);
+        return view;
+    }
+
+    private static String generateCode() {
+        StringBuilder sb = new StringBuilder();
+        Random r = new Random();
+        for (int i = 0; i < 30; i++) {
+            sb.append(r.nextInt(101));
+        }
+        return sb.toString();
+    }
+    Color a = new Color();
+    public static void refrescarQR() {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        String s = generateCode();
+
+        try {
+            BitMatrix bitMatrix = qrCodeWriter.encode(s, BarcodeFormat.QR_CODE, 450, 450);
+            Bitmap bitmap = Bitmap.createBitmap(450, 450, Bitmap.Config.RGB_565);
+            for (int x = 0; x < 450; x++) {
+                for (int y = 0; y < 450; y++) {
+                    bitmap.setPixel(x, y, bitMatrix.get(x, y) ? Color.BLACK : Color.parseColor("#E5F1FF"));
+                }
+            }
+            qrImageView.setImageBitmap(bitmap);
+        } catch (WriterException e) {
+            Log.e(TAG, e.getLocalizedMessage());
+
+        }
+
     }
 }
